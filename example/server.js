8edd534144b057b2a61a4197a40474e6ff330484
Rob Foley <rob.foley.jr@gmail.com>
2016-07-12 15:51:37 -0400

    Fixed error with CORS requests.

--- example/server.js
+++ example/server.js
@@ -35,8 +35,22 @@ var firstLog = true;
 var app = express();
 
 app.set('port', process.env.PORT || 8000);
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+});
 app.use(bodyParser.urlencoded({extended: true, limit: '100mb'}));
 app.use(bodyParser.json({limit: '100mb'}));
+
 
 app.get('/', function (req, res) {
   res.send('UserAle Local');
