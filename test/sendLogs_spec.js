c610177f156a9d2f71c32cc7991ae7aa4b24d846
Rob Foley <rob.foley.jr@gmail.com>
2016-09-21 17:03:02 -0400

    Added tests for sendLogs.

--- test/sendLogs_spec.js
+++ test/sendLogs_spec.js
@@ -16,9 +16,100 @@
  */
 import { expect } from 'chai';
 import jsdom from 'jsdom';
-import fs from 'fs';
+import sinon from 'sinon';
+import { sendOnInterval, sendOnClose } from '../src/sendLogs';
 
 describe('sendLogs', () => {
-  it('sends logs on an interval');
-  it('sends logs on page exit');
+  it('sends logs on an interval', (done) => {
+    let requests = 0;
+    const originalXMLHttpRequest = global.XMLHttpRequest;
+    const conf = { transmitInterval: 500, url: 'test', logCountThreshold: 2 };
+    const logs = [];
+    const clock = sinon.useFakeTimers();
+    const xhr = sinon.useFakeXMLHttpRequest();
+    global.XMLHttpRequest = xhr;
+    xhr.onCreate = () => { requests++; };
+
+    sendOnInterval(logs, conf);
+
+    clock.tick(conf.transmitInterval * 2);
+    // Make sure it doesn't make requests for no raisin
+    expect(requests).to.equal(0);
+
+    // Make sure it respects the logCountThreshold
+    logs.push({ foo: 'bar1' });   
+    clock.tick(conf.transmitInterval);
+    expect(logs.length).to.equal(1);
+
+    // Make sure it sends the logs and clears the array
+    logs.push({ foo: 'bar2' });
+    clock.tick(conf.transmitInterval);
+    expect(logs.length).to.equal(0);
+    expect(requests).to.equal(1);
+
+    xhr.restore();
+    clock.restore();
+    global.XMLHttpRequest = originalXMLHttpRequest;
+    done();
+  });
+  it('sends logs on page exit with navigator', (done) => {
+    const html = `<html><head></head><body></body></html>`;
+    jsdom.env({
+      html,
+      done: (err, window) => {
+        const originalNavigator = global.navigator;
+        const originalWindow = global.window;
+        let called = false;
+        global.window = window;
+        global.navigator = {
+          sendBeacon: () => {
+            called = true;
+          },
+        };
+
+        const evt = window.document.createEvent('CustomEvent');
+        evt.initEvent('unload', true, true);
+        sendOnClose([{ foo: 'bar' }], { url: 'test' });
+
+        window.dispatchEvent(evt);
+        window.close();
+
+        expect(called).to.equal(true);
+        global.window = originalWindow;
+        global.navigator = originalNavigator;
+        done();
+      }
+    });
+  });
+  it('sends logs on page exit without navigator', (done) => {
+    const html = `<html><head></head><body></body></html>`;
+    jsdom.env({
+      html,
+      done: (err, window) => {
+        const originalNavigator = global.navigator;
+        const originalXMLHttpRequest = global.XMLHttpRequest;
+        const originalWindow = global.window;
+        let requests = 0;
+        const xhr = sinon.useFakeXMLHttpRequest();
+        global.XMLHttpRequest = xhr;
+        global.window = window;
+        global.XMLHttpRequest = xhr;
+        global.navigator = { sendBeacon: false, };
+        xhr.onCreate = () => { requests++; };
+
+        const evt = window.document.createEvent('CustomEvent');
+        evt.initEvent('beforeunload', true, true);
+        sendOnClose([{ foo: 'bar' }], { url: 'test' });
+        
+        window.dispatchEvent(evt);
+        window.close();
+
+        expect(requests).to.equal(1);
+        global.window = originalWindow;
+        global.navigator = originalNavigator;
+        global.XMLHttpRequest = originalXMLHttpRequest;
+        done();
+      }
+    });
+  });
 });
