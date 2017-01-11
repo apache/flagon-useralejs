08dac57a4e41e87aa58da6a2203836a7b6c80c57
Clay Gimenez <cgimenez@draper.com>
2017-01-11 16:58:37 -0500

    ASF Website demo modifications

--- src/sendLogs.js
+++ src/sendLogs.js
@@ -5,9 +5,9 @@
  * The ASF licenses this file to You under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
- * 
+ *
  *   http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,6 +22,11 @@ export function initSender(logs, config) {
 
 export function sendOnInterval(logs, config) {
   setInterval(function() {
+    // Demo capability to not send logs
+    if (config.noSend) {
+      return false;
+    }
+
     if (logs.length >= config.logCountThreshold) {
       sendLogs(logs.slice(0), config.url, 0); // Send a copy
       logs.splice(0); // Clear array reference (no reassignment)
@@ -32,14 +37,24 @@ export function sendOnInterval(logs, config) {
 export function sendOnClose(logs, config) {
   if (navigator.sendBeacon) {
     window.addEventListener('unload', function() {
+      // Demo capability to not send logs
+      if (config.noSend) {
+        return false;
+      }
+
       navigator.sendBeacon(config.url, JSON.stringify(logs));
     });
   } else {
     window.addEventListener('beforeunload', function() {
       if (logs.length > 0) {
+        // Demo capability to not send logs
+        if (config.noSend) {
+          return false;
+        }
+
         sendLogs(logs, config.url, 1);
       }
-    })
+    });
   }
 }
 
