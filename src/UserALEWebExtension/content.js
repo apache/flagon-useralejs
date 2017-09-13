4b7adcc7903d5530e02e15011e237bfc7e69c22f
Rob Foley <rob.foley.jr@gmail.com>
2017-09-13 16:01:36 -0700

    Updated to use global session id

--- src/UserALEWebExtension/content.js
+++ src/UserALEWebExtension/content.js
@@ -6,8 +6,6 @@ import * as globals from './globals';
 import * as MessageTypes from './messageTypes.js';
 import { map, options, start } from '../main.js';
 
-// inherent dependency on globals.js, loaded by the webext
-
 // browser is defined in firefox, but not in chrome. In chrome, they use
 // the 'chrome' global instead. Let's map it to browser so we don't have
 // to have if-conditions all over the place.
@@ -17,6 +15,7 @@ var browser = browser || chrome;
 // creates a Future for retrieval of the named keys
 // the value specified is the default value if one doesn't exist in the storage
 let store = browser.storage.local.get({
+  sessionId: null,
   userAleHost: globals.userAleHost,
   userAleScript: globals.userAleScript,
   toolUser: globals.toolUser,
@@ -28,6 +27,7 @@ function storeCallback(item) {
   injectScript({
     url: item.userAleHost,
     userId: item.toolUser,
+    sessionID: item.sessionId,
     toolName: item.toolName,
     toolVersion: item.toolVersion
   });
