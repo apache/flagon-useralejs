e69812c44f0ebb35ee61f3216c2a0d7cd48646db
Rob Foley <rob.foley.jr@gmail.com>
2017-09-13 14:18:48 -0700

    Updated to log via the content script, without injecting a tag. Switched to using background/content scripts

--- src/UserALEWebExtension/options.js
+++ src/UserALEWebExtension/options.js
@@ -1,46 +1,50 @@
 /* eslint-disable */
+import * as globals from './globals.js';
+import * as MessageTypes from './messageTypes.js';
 
 if (chrome) {
-    browser = chrome;
+  browser = chrome;
 }
 
 // creates a Future for retrieval of the named keys
 // the value specified is the default value if one doesn't exist in the storage
 let store = browser.storage.local.get({
-    userAleHost: userAleHost,
-    userAleScript: userAleScript,
-    toolUser: toolUser,
-    toolName: toolName,
-    toolVersion: toolVersion,
+  userAleHost: globals.userAleHost,
+  userAleScript: globals.userAleScript,
+  toolUser: globals.toolUser,
+  toolName: globals.toolName,
+  toolVersion: globals.toolVersion,
 }, storeCallback);
-        
+
 function storeCallback(item) {
-    console.log(item);
-    document.getElementById("host").value = item.userAleHost;
-    document.getElementById("clientScript").value = item.userAleScript;
-    document.getElementById("toolUser").value = item.toolUser;
-    document.getElementById("toolName").value = item.toolName;
-    document.getElementById("toolVersion").value = item.toolVersion;
+  console.log(item);
+  document.getElementById("host").value = item.userAleHost;
+  document.getElementById("clientScript").value = item.userAleScript;
+  document.getElementById("toolUser").value = item.toolUser;
+  document.getElementById("toolName").value = item.toolName;
+  document.getElementById("toolVersion").value = item.toolVersion;
 }
-        
+
 function onError(error) {
-    console.log(error);
+  console.log(error);
 }
 
 function saveOptions(e) {
-    //e.preventDefault();
-    browser.storage.local.set({
-        userAleHost: document.getElementById("host").value,
-        userAleScript: document.getElementById("clientScript").value,
-        toolUser: document.getElementById("toolUser").value,
-        toolName: document.getElementById("toolName").value,
-        toolVersion: document.getElementById("toolVersion").value,
+  const updatedConfig = {
+    userAleHost: document.getElementById("host").value,
+    userAleScript: document.getElementById("clientScript").value,
+    toolUser: document.getElementById("toolUser").value,
+    toolName: document.getElementById("toolName").value,
+    toolVersion: document.getElementById("toolVersion").value,
+  };
 
-    });
+  browser.storage.local.set(updatedConfig);
+
+  browser.runtime.sendMessage({ type: MessageTypes.CONFIG_CHANGE, payload: updatedConfig });
 }
 
 document.addEventListener("submit", function() {
-    saveOptions();
+  saveOptions();
 });
 
-/* eslint-enable */
+  /* eslint-enable */
