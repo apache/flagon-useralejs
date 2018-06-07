738319bacabd1d010a5044ae30ae3322b7d78e91
Rob Foley <rgf3059@draper.com>
2018-06-07 12:42:26 -0400

    Switched to using test utils (fixes broken tests)

--- test/main_spec.js
+++ test/main_spec.js
@@ -18,6 +18,8 @@ import { expect } from 'chai';
 import jsdom from 'jsdom';
 import fs from 'fs';
 
+import { createEnv } from './testUtils';
+
 describe('Userale API', () => {
   const url = 'file://' + __dirname + '/main.html';
   const html = fs.readFileSync(__dirname + '/main.html');
@@ -27,84 +29,72 @@ describe('Userale API', () => {
   };
 
   it('provides configs', (done) => {
-    jsdom.env({
-      html, url, features,
-      done : (err, window) => {
-        const config = window.userale.options();
-        expect(config).to.be.an('object');
-        expect(config).to.have.all.keys([
-          'on',
-          'useraleVersion',
-          'autostart',
-          'url',
-          'transmitInterval',
-          'logCountThreshold',
-          'userId',
-          'version',
-          'logDetails',
-          'resolution',
-          'toolName',
-          'userFromParams',
-          'time',
-        ]);
-        window.close();
-        done();
-      }
+    createEnv(html, (err, window) => {
+      const config = window.userale.options();
+      expect(config).to.be.an('object');
+      expect(config).to.have.all.keys([
+        'on',
+        'useraleVersion',
+        'autostart',
+        'url',
+        'transmitInterval',
+        'logCountThreshold',
+        'userId',
+        'version',
+        'logDetails',
+        'resolution',
+        'toolName',
+        'userFromParams',
+        'time',
+      ]);
+      window.close();
+      done();
     });
   });
 
   it('edits configs', (done) => {
-    jsdom.env({
-      html, url, features,
-      done : (err, window) => {
-        const config = window.userale.options();
-        const interval = config.transmitInterval;
-        window.userale.options({
-          transmitInterval : interval + 10
-        });
-        const newConfig = window.userale.options();
+    createEnv(html, (err, window) => {
+      const config = window.userale.options();
+      const interval = config.transmitInterval;
+      window.userale.options({
+        transmitInterval : interval + 10
+      });
+      const newConfig = window.userale.options();
 
-        expect(newConfig.transmitInterval).to.equal(interval + 10);
-        window.close();
-        done();
-      }
+      expect(newConfig.transmitInterval).to.equal(interval + 10);
+      window.close();
+      done();
     });
   });
 
   it('starts + stops', (done) => {
-    jsdom.env({
-      html, url, features,
-      done : (err, window) => {
-        setTimeout(() => {
-          const { userale } = window;
-          expect(userale.options().on).to.equal(true);
+    createEnv(html, (err, window) => {
+      setTimeout(() => {
+        const { userale } = window;
+        expect(userale.options().on).to.equal(true);
 
-          userale.stop();
-          expect(userale.options().on).to.equal(false);
+        userale.stop();
+        expect(userale.options().on).to.equal(false);
 
-          userale.start();
-          expect(userale.options().on).to.equal(true);
+        userale.start();
+        expect(userale.options().on).to.equal(true);
 
-          window.close();
-          done();
-        }, 200);
-      }
+        window.close();
+        done();
+      }, 200);
     });
   });
 
   it('sends custom logs', (done) => {
-    jsdom.env({
-      html, url, features,
-      done: (err, window) => {
-        const { userale } = window;
+    createEnv(html, (err, window) => {
+      const { userale } = window;
 
-        expect(userale.log({})).to.equal(true);
-        expect(userale.log()).to.equal(false);
-        expect(userale.log(null)).to.equal(false);
+      expect(userale.log({})).to.equal(true);
+      expect(userale.log()).to.equal(false);
+      expect(userale.log(null)).to.equal(false);
 
-        window.close();
-        done();
-      }
+      window.close();
+      done();
     });
   });
 });
