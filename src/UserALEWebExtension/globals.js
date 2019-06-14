11e4b29e5704d667ed0769a6164ebf9fd24bd804
poorejc <poorejc@apache.org>
2019-06-14 16:01:23 -0400

    [FLAGON-408] Updated webextension src to reflect new useralejs version

--- src/UserALEWebExtension/globals.js
+++ src/UserALEWebExtension/globals.js
@@ -19,9 +19,9 @@
 
 // these are default values, which can be overridden by the user on the options page
 export var userAleHost = 'http://localhost:8000';
-export var userAleScript = 'userale-0.2.1.min.js';
+export var userAleScript = 'userale-2.0.0.min.js';
 export var toolUser = 'nobody';
 export var toolName = 'test_app';
-export var toolVersion = '0.1.0';
+export var toolVersion = '2.0.0';
 
 /* eslint-enable */
