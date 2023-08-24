756c697a4dbee55c202fea00fed81cf3bfcbe7be
Jason Young <jason_y54@protonmail.com>
2023-08-24 13:39:48 -0400

    Disable video

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
+  video: false,
   e2e: {
     specPattern: 'journey/**/*.cy.{js,jsx,ts,tsx}',
     supportFile: false
